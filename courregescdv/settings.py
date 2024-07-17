4d63fb10682fa72c81af10bf8f507d1de2fabdcd
bsozeau <107618870+bsozeau@users.noreply.github.com>
2024-07-17 15:23:26 +0200

    Update settings.py

--- courregescdv/settings.py
+++ courregescdv/settings.py
@@ -77,19 +77,19 @@ WSGI_APPLICATION = 'courregescdv.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+#DATABASES = {
+ #   'default': dj_database_url.config(
+  #      # Replace this value with your local database's connection string.
+   #     default='postgresql://postgres:postgres@localhost:5432/mysite',
+    #    conn_max_age=600
+    #)
+#}
 
 
 # Password validation
